1c93bdcd464bbbe57072f02273b1c0a2c5b1b8ef
bobo <bigbo_1002e@outlook.com>
2021-04-11 13:28:13 +0800

    cglib实现静态代理；新增Maven工程

---
+++ src/main/java/proxy/cglib/Main.java
@@ -0,0 +1,44 @@
+package proxy.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Tank.class);  //设置父类
+        enhancer.setCallback(new TimeMehodInterceptor());  //设定回调
+        Tank tank = (Tank) enhancer.create();
+        tank.move();
+    }
+}
+
+// 回调器 - 相当于jdk动态代理中的InvocationHandler
+class TimeMehodInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("before");
+        Object result = null;
+        result = methodProxy.invokeSuper(o, objects);
+        System.out.println("after");
+        return result;
+    }
+}
+
+
+class Tank{
+    public void move() {
+
+        System.out.println("moving~");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
