5a077d2192d5ccb4cc742dc30991a2d60ca95507
bobo <bigbo_1002e@outlook.com>
2021-04-19 00:19:16 +0800

    BIO,NIO,Netty; netty初步实现群发功能

---
+++ src/main/java/NettyStudy/bio/IOClient.java
@@ -0,0 +1,26 @@
+package NettyStudy.bio;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+public class IOClient {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 3333);
+                while (true){
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello~").getBytes());
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+}
---
+++ src/main/java/NettyStudy/netty/Server.java
@@ -0,0 +1,86 @@
+package NettyStudy.netty;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.io.IOException;
+
+public class Server {
+
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    public static void main(String[] args) throws IOException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);   //餐厅管家  accept
+        EventLoopGroup workerGroup = new NioEventLoopGroup(1);    //餐厅服务员  worker
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pl = socketChannel.pipeline();
+                            pl.addLast(new ServerChildHandler());
+                            System.out.println(socketChannel);
+                        }
+                    })  //
+                    .bind(8888)
+                    .sync();
+
+            System.out.println("server started!");
+
+            f.channel().closeFuture().sync();   //clost()->ChannelFuture  ,调用close时餐厅才会关门
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
+
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try{
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), bytes);
+            System.out.println(new String(bytes));
+
+            //ctx.writeAndFlush(msg);  // 如果只是读，需要写finally释放；如果还有写操作，用这个就可以不用finally释放了
+            Server.clients.writeAndFlush(msg);
+            //System.out.println(buf);
+        }finally {
+//            if(buf!=null){
+//                ReferenceCountUtil.release(buf);
+//                //System.out.println(buf.refCnt());
+//            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
